e5287d41e514eb1329e617091f7c60f4fa8545ca
Paulos Dessie Nedaw <pauldessie@gmail.com>
2022-10-14 14:44:26 +0300

    crated a separate action file

--- src/Context/github/GithubUserAction.js
+++ src/Context/github/GithubUserAction.js
@@ -1,38 +1,38 @@
+import axios from "axios"
+
+const github = axios.create({
+    baseURL: process.env.REACT_APP_GITHUB_URL,
+    // headers: {
+    //     Authorization: `token ${process.env.REACT_APP_GITHUB_TOKEN}` 
+    // }
+})
 
 export const getUser = async(login) => {
     
-    const response = await fetch(`${process.env.REACT_APP_GITHUB_URL}/users/${login}`, {
-      headers: {
-          Authorization: `token ${process.env.REACT_APP_GITHUB_TOKEN}` 
-      }
-    }) 
+    const response = await github.get(`/users/${login}`) 
     if (response.status === 404){
       window.location = '/notfound'
     } else {
-      const data = await response.json()
-      return data
+      return response.data
     }
   }
 
 
 export const getUserRepos = async (login) => {
-    const response = await fetch(`${process.env.REACT_APP_GITHUB_URL}/users/${login}/repos`, {
-      headers: {
-          Authorization: `token ${process.env.REACT_APP_GITHUB_TOKEN}` 
-      }
-    }) 
-      const data = await response.json()
-      return data
+    const response = await github.get(`/users/${login}/repos`)
+    if (response.status === 404){
+        return []
+    }
+    return response.data
   }
 
 
 export const searchUsers = async(term) => {
-    const response = await fetch(`${process.env.REACT_APP_GITHUB_URL}/search/users?q=${term}`, {
-      headers: {
-          Authorization: `token ${process.env.REACT_APP_GITHUB_TOKEN}` 
+    const response = await github.get(`/search/users`, { params: { q: term } })
+    console.log(response.data)
+    if (response.status === 404){
+        window.location = '/notfound'
+      } else {
+        return response.data
       }
-    })
-
-    const data = await response.json()
-    return data
   }
